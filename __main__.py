1bc6ccf4ae281c676b6e3b828f118ec815ee0c3e
sharan-dce <sharan20051998@gmail.com>
2020-02-05 22:47:40 +0530

    Fixed params

--- __main__.py
+++ __main__.py
@@ -9,24 +9,12 @@ from network import ActorCritic
 from train import run_training_procedure
 from utils import process_screen
 
-def get_optimizer(optimizer_name):
-
-	if optimizer_name == 'sgd':
-		return tf.keras.optimizers.SGD
-	elif optimizer_name == 'adam':
-		return tf.keras.optimizers.Adam
-	elif optimizer_name == 'rms_prop':
-		return tf.keras.optimizers.RMSprop
-	else:
-		print('Unknown optimizer ' + optimizer_name)
-		quit()
 
 if __name__ == '__main__':
 	from argparse import ArgumentParser
 	argparse = ArgumentParser()
 	argparse.add_argument('--learning_rate', type = float)
 	argparse.add_argument('--environment', type = str)
-	argparse.add_argument('--optimizer', type = str)
 	argparse.add_argument('--gamma', type = float)
 	argparse.add_argument('--checkpoint_dir', action = readable_dir)
 	argparse.add_argument('--gifs_dir', action = readable_dir)
@@ -44,7 +32,7 @@ if __name__ == '__main__':
 	print('Creating {} environments for parallel processing'.format(args.threads))
 	args.environments = [gym.make(args.environment) for _ in range(args.threads)]
 
-	args.optimizer = get_optimizer(args.optimizer)(args.learning_rate)
+	args.optimizer = tf.keras.optimizers.RMSprop(args.learning_rate, 0.99)
 	args.actor_critic = ActorCritic(args.environments[0].action_space.n)
 	args.actor_critic.set_threads(args.threads)
 	args.target_network = args.actor_critic
