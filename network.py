Fixed some parameters and added gradient clipping

network.py
@@ -8,13 +8,12 @@ class ActorCritic():
         self.n_actions = n_actions
 
         self._layers = []
-        self._layers.append(layers.Conv2D(filters = 16, kernel_size = 7, strides = 3, activation = tf.nn.leaky_relu))
-        self._layers.append(layers.Conv2D(filters = 32, kernel_size = 7, strides = 3, activation = tf.nn.leaky_relu))
-        self._layers.append(layers.MaxPool2D(2, strides = 2, padding = 'valid'))
+        self._layers.append(layers.Conv2D(filters = 16, kernel_size = 8, strides = 4, activation = tf.nn.leaky_relu))
+        self._layers.append(layers.Conv2D(filters = 32, kernel_size = 4, strides = 2, activation = tf.nn.leaky_relu))
         self._layers.append(layers.Reshape((1, -1)))
 
-        self._layers.append(layers.GRU(2048, return_state = True, return_sequences = True))
-        self._layers.append(layers.GRU(1024, return_state = True))
+        self._layers.append(layers.GRU(512, return_state = True, return_sequences = True))
+        self._layers.append(layers.GRU(128, return_state = True))
 
         self._actor = layers.Dense(self.n_actions, activation = tf.nn.softmax)
         self._critic = layers.Dense(1, activation = None)